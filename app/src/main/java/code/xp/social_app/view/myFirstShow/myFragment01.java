d9c25d9827958f3bef9c59b02e1baf6a7c9fc79e
X970805 <958868471@qq.com>
2017-12-13 21:12:44 +0800

    mymessage

---
+++ app/src/main/java/code/xp/social_app/view/myFirstShow/myFragment01.java
@@ -0,0 +1,25 @@
+package code.xp.social_app.view.myFirstShow;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import code.xp.social_app.R;
+
+/**
+ * Created by 徐宏福 on 2017/12/13.
+ */
+
+public class myFragment01 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = View.inflate(getContext(), R.layout.shoufragment, null);
+        //
+        return view;
+
+    }
+}
